bfe564af666e7151eb062163d89ab85978e88053
lgimenez-code <lucasgimenez2007@gmail.com>
2026-01-23 15:16:55 -0300

    desactivar animacion de numeros

--- src/pages/home/stats-section.tsx
+++ src/pages/home/stats-section.tsx
@@ -10,7 +10,7 @@ export function StatsSection() {
     <section ref={sectionRef} className="py-15 bg-dark-blue text-white">
       <div className="container-custom">
         <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-12 text-center">
-          {STATS.map((stat) => {
+          {/* {STATS.map((stat) => {
             const { prefix, number, suffix } = parseStatValue(stat.value)
             
             return (
@@ -37,7 +37,17 @@ export function StatsSection() {
                 <p className="text-lg opacity-90 font-nunito">{stat.label}</p>
               </div>
             )
-          })}
+          })} */}
+
+          {/*  version estatica */}
+          {STATS.map((stat) => (
+            <div key={stat.label}>
+              <h3 className="text-5xl font-bold mb-2 flex items-center justify-center">
+                {stat.value}
+              </h3>
+              <p className="text-lg opacity-90 font-nunito">{stat.label}</p>
+            </div>
+          ))}
         </div>
       </div>
     </section>
