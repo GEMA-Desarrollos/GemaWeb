bfdd5c2a765aa4d5b49e5e609f1a3c39a9b99b83
lgimenez-code <lucasgimenez2007@gmail.com>
2026-01-28 16:33:23 -0300

    cambios de build en vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,12 +3,30 @@ import tailwindcss from "@tailwindcss/vite"
 import react from "@vitejs/plugin-react"
 import { defineConfig } from "vite"
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
+  },
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          react: ["react", "react-dom"],
+          router: ["react-router-dom"],
+          motion: ["framer-motion"],
+          animation: ["lottie-react"],
+          confetti: ["react-confetti"],
+          typewriter: ["typewriter-effect"],
+          radix: ["radix-ui", "@base-ui/react"],
+          carousel: ["embla-carousel-react", "embla-carousel-autoplay"],
+          icons: ["lucide-react"],
+          utils: ["class-variance-authority", "clsx", "tailwind-merge"]
+        },
+      }
+    },
+    chunkSizeWarningLimit: 500,
   },
 })
